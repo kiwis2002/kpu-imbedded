a8e10eb2737623fc016a5099bcc32f94970bdc17
E423-17 <E423-17>
2016-03-28 20:31:50 +0900

    add hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,4 @@
-main(){
-	printf("Hello Alex World!\n");
-	setuid(0);
-	setgid(0);
-	system("/bin/bash");
-	/* printf(" I'm root, that is, Super User!!!\n"); delete this line */
+main()
+{
+printf("I want go home\n");
 }
